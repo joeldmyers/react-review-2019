01e7b92632c7c90b190db5b4b4cc4c8e31ee7954
Joel Myers <joeldmyers@gmail.com>
2019-10-27 14:38:58 -0400

    bugfix

--- adopt-me/src/js/useDropdown.js
+++ adopt-me/src/js/useDropdown.js
@@ -6,9 +6,13 @@ const useDropdown = (label, defaultState, options) => {
   const Dropdown = () => (
     <label htmlFor={id}>
       {label}
-      <select id={id} value={state} onChange={e => setState(e.target.value)}>
+      <select
+        id={id}
+        value={state}
+        onChange={e => setState(e.target.value)}
         onBlur={e => setState(e.target.value)}
         disabled={options.length === 0}
+      >
         <option>All</option>
         {options.map(item => (
           <option key={item} value={item}>
