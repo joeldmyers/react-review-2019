de4241d85bb6d312caa73acb3157eea10ac93c6b
Joel Myers <joeldmyers@gmail.com>
2019-10-27 12:23:45 -0400

    adding useEffects example to handle lifecycle change

--- adopt-me/src/js/SearchParams.js
+++ adopt-me/src/js/SearchParams.js
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle, WA");
+  const [location, setLocation] = useState("San Francisco, CA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  // schedules to run after - everything is rendered first, then
+  // useEffect is run.  It's equivalent to componentDidMount, componentDidUpdate.
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      // returns an array of data, and then we pull the name off each one.
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]); // this is the list of what specifically
+  // React needs to check as to whether it has changed.
+  //Otherwise it is checking for everything.
 
   return (
     <div className="search-params">
